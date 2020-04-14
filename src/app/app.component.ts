91baddf4cdf9e0173afbc55efcaf7de3ba9f0579
Kapilca <macbookssd@Kapilca-MacPro.local>
2020-04-14 10:23:05 -0300

    Ínclusao de Alerta&Confirmacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarService } from './services/car.service';
+import { Car } from './models/car';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-http';
+export class AppComponent implements OnInit {
+
+  filter: string = '';
+  // Configuração da ordenação
+  key: string = 'model'; // Define um valor padrão, para quando inicializar o componente
+  reverse: boolean = false;
+  sort(key) {
+      this.key = key;
+      this.reverse = !this.reverse;
+  }
+  public paginaAtual = 1; // Dizemos que queremos que o componente quando carregar, inicialize na página 1.
+  car = {} as Car;
+  cars: Car[];
+  
+
+  constructor(private carService: CarService) {}
+  
+  ngOnInit() {
+    this.getCars();
+  }
+
+  // defini se um carro será criado ou atualizado
+  saveCar(form: NgForm) {
+    if (this.car.id !== undefined) {
+      this.carService.updateCar(this.car).subscribe(() => {
+        this.cleanForm(form);
+      });
+    } else {
+      this.carService.saveCar(this.car).subscribe(() => {
+        this.cleanForm(form);
+      });
+    }
+  }
+
+  // Chama o serviço para obtém todos os carros
+  getCars() {
+    this.carService.getCars().subscribe((cars: Car[]) => {
+      this.cars = cars;
+    });
+  }
+
+  // deleta um carro
+  deleteCar(car: Car) {
+    this.carService.deleteCar(car).subscribe(() => {
+      this.getCars();
+    });
+  }
+
+  // copia o carro para ser editado.
+  editCar(car: Car) {
+    this.car = { ...car };
+  }
+
+  // limpa o formulario
+  cleanForm(form: NgForm) {
+    this.getCars();
+    form.resetForm();
+    this.car = {} as Car;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination'; // Módulo da dependência de paginação/
+import { OrderModule } from 'ngx-order-pipe';  // Módulo da dependência de paginação
+import { Ng2SearchPipeModule } from 'ng2-search-filter'; // Importação
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgxPaginationModule,
+    OrderModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
